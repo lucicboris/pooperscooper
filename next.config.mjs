ab621f901339072564e69186db24cd4af664bf86
Boris Lucic <lucicboris59@gmail.com>
2024-06-09 17:23:19 +0900

    init commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+};
+
+export default nextConfig;
